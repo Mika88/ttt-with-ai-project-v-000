1f56af3a52a27481a3fdf5b5702b633bf34e32a9
Mika88 <mikaspivak@gmail.com>
2018-08-28 23:07:04 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -9,7 +9,7 @@ module Players
           elsif w.select{|i| board.position(i + 1) != " " && board.position(i + 1) != token}.size == 2 && w.any?{|i| board.position(i + 1) == " "}
             input = w.select{|i| !board.taken?(i+1)}.first.to_i.+(1).to_s
           elsif input == nil
-             input = ["5", "1", "3", "7", "9", "2", "4", "6", "8"].find{|i| !board.taken?(i + 1)}
+             input = ["5", "1", "3", "7", "9", "2", "4", "6", "8"].find{|i| !board.taken?(i + 1)}.to_s
           end
         end
       input
