cbb58fc78468c30115710ffbdd86fd8ac9baf8e9
Mika88 <mikaspivak@gmail.com>
2018-08-28 23:01:34 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -3,13 +3,6 @@ module Players
   class Computer < Player
     def move(board)
        input = nil
-       if !board.taken?("5")
-        input = "5"
-       elsif board.turn_count == 1
-        input = "1"
-       elsif board.turn_count == 2
-        input = ["1", "3", "7", "9"].detect{|i| !board.taken?(i)}
-       else
         Game::WIN_COMBINATIONS.find do |w|
           if w.select{|i| board.position(i + 1) == token}.size == 2 && w.any?{|i| board.position(i + 1) == " "}
             input = w.select{|i| !board.taken?(i + 1)}.first.to_i.+(1).to_s
